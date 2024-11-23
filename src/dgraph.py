d36d3f09e9a4f42dc26e894f70eb779573ba1bd6
AlexToral <jesus.toral@iteso.mx>
2024-11-23 14:11:18 -0600

    Se aseguro de que un solo uid exista para cada nodo de curso y user

--- src/dgraph.py
+++ src/dgraph.py
@@ -50,14 +50,85 @@ def loadSchema(client):
     except Exception as e:
         print(f"Error en el esquema DGRAPH: {e}")
 
-def loadData():
-    connection = DBconnections.DGRAPH
-    data = DBconnections.DATA
-    i = 0
-    for user in data.get("users"):
-        print(user['follows'])
-        i+=1
+def obtainUser(user):
+    id = user["id"]
+    name = user["name"]
+    email = user["email"]
+    phone = user["phone_number"]
+    likeList = []
+    for like in user["likes"]:
+        likeList.append(like)
 
-    print(i)
+    upload_user(id,name,email,phone,likeList)
+
+def checkUID(uid):
+    connection = DBconnections.DGRAPH
+
+    query = """query user_exists($a: string){
+        all(func: eq(c_uid,$a)){
+        uid
+        }
+    }
+    """
+
+    variables = {'$a': uid}
+    res = connection.txn(read_only=True).query(query, variables=variables)
+    user = json.loads(res.json)
+
+    if "all" in user and len(user["all"]) > 0:
+        return user["all"][0]["uid"]
+    else:
+        return None 
+
+def upload_user(id,name,email,phone,likeList):
+    connection = DBconnections.DGRAPH
+    txn = connection.txn()
+
+    result = checkUID(id)
+
+    if result is not None:
+        user_uid = result
+    else:
+        user_uid = "_:new_obj"
+
+    processed_likes = []
+    for like in likeList:
+        course_uid = checkUID(like)
+        if course_uid is None:
+            course_uid = "_:course_" + like  # Create a blank node for a new course
+        processed_likes.append({"uid": course_uid, "c_uid": like, "dgraph.type": "course"})
+
+    try:
+
+        user = {
+            "uid":user_uid,
+            "c_uid":id,
+            "dgraph.type":"user",
+            "name":name,
+            "email":email,
+            "phone_number":phone,
+            "likes":processed_likes
+        }
+
+        txn.mutate(set_obj=user)
+
+        commit_response = txn.commit()
+        print(f"Commit Response: {commit_response}")
+    except Exception as e:
+        print(f"Error during upsert: {e}")
+    finally:
+        txn.discard()
+
+
+
+def loadData():
+    
+    data = DBconnections.DATA
+    
+    for user in data.get("users"):
+        obtainUser(user)
+
+
 def deleteData():
-    pass
+    connection = DBconnections.DGRAPH
+    return connection.alter(pydgraph.Operation(drop_all=True))
