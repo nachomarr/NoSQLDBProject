627d37d0b80572e6bcf8a739d3176f1969fde8ae
AlexToral <jesus.toral@iteso.mx>
2024-11-23 23:30:11 -0600

    Se acabo de subir todos los datos a dgraph

--- src/dgraph.py
+++ src/dgraph.py
@@ -9,11 +9,10 @@ def loadSchema(client):
     type user {
         c_uid
         name
-        email
-        phone_number
         likes
         dislikes
         suscribed
+        follows
         teaches
     }  
 
@@ -21,16 +20,14 @@ def loadSchema(client):
         c_uid
         name
         description
-        start_date
-        end_date
         regristation_spaces
         keywords
         mode
 
     }
 
-    c_uid: string @index(exact) .
-    name: string .
+    c_uid: string @index(hash) .
+    name: string  @index(exact).
     email: string .
     phone_number: string .
     likes: [uid] . 
@@ -38,11 +35,10 @@ def loadSchema(client):
     suscribed: [uid] .
     teaches: [uid] .  
     description: string .
-    start_date: datetime .
-    end_date: datetime .
     keywords: [string] .
     mode: string .
     regristation_spaces: int .
+    follows: [uid] .
     """
     try:
         client.alter(pydgraph.Operation(schema=schema))
@@ -53,18 +49,44 @@ def loadSchema(client):
 def obtainUser(user):
     id = user["id"]
     name = user["name"]
-    email = user["email"]
-    phone = user["phone_number"]
     likeList = []
+    dislikeList = []
+    suscribedList = []
+    teachingList = []
+    followsList = []
+
     for like in user["likes"]:
         likeList.append(like)
+    
+    for dislike in user["dislikes"]:
+        dislikeList.append(dislike)
+    
+    for suscription in user["suscribed"]:
+        suscribedList.append(suscription["course_id"])
+    
+    for teaching in user["teaches"]:
+        teachingList.append(teaching)
 
-    upload_user(id,name,email,phone,likeList)
+    for follow in user["follows"]:
+        followsList.append(follow)
+
+    upload_user(id,name,likeList,dislikeList,suscribedList,teachingList,followsList)
+
+def obtainCourse(course):
+    id = course["id"]
+    nameCourse = course["name"]
+    descriptionCourse = course["description"]
+    spaces = course["registration_spaces"]
+    keywords = course["keywords"]
+    mode = course["mode"]
+
+    upload_course(id,nameCourse,descriptionCourse,spaces,keywords,mode)
+    
 
 def checkUID(uid):
     connection = DBconnections.DGRAPH
 
-    query = """query user_exists($a: string){
+    query = """query id_exists($a: string){
         all(func: eq(c_uid,$a)){
         uid
         }
@@ -80,7 +102,18 @@ def checkUID(uid):
     else:
         return None 
 
-def upload_user(id,name,email,phone,likeList):
+def proccess_data(list,dgraph_type):
+    proccessed_list = []
+    for obj in list:
+        obj_uid = checkUID(obj)
+        if obj_uid is None:
+            obj_uid = "_:new_obj_" + obj
+        proccessed_list.append({"uid": obj_uid, "c_uid": obj, "dgraph.type": dgraph_type})
+
+    return proccessed_list
+
+
+def upload_user(id,name,likeList,dislikeList,suscribedList,teachingList,followsList):
     connection = DBconnections.DGRAPH
     txn = connection.txn()
 
@@ -91,31 +124,58 @@ def upload_user(id,name,email,phone,likeList):
     else:
         user_uid = "_:new_obj"
 
-    processed_likes = []
-    for like in likeList:
-        course_uid = checkUID(like)
-        if course_uid is None:
-            course_uid = "_:course_" + like  # Create a blank node for a new course
-        processed_likes.append({"uid": course_uid, "c_uid": like, "dgraph.type": "course"})
-
+    processed_likes = proccess_data(likeList,"course")
+    processed_dislikes = proccess_data(dislikeList,"course")
+    processed_suscribed = proccess_data(suscribedList,"course")
+    proccessed_teaching = proccess_data(teachingList,"course")
+    proccessed_follows = proccess_data(followsList,"user")
+    
     try:
-
         user = {
             "uid":user_uid,
             "c_uid":id,
             "dgraph.type":"user",
             "name":name,
-            "email":email,
-            "phone_number":phone,
-            "likes":processed_likes
+            "likes":processed_likes,
+            "dislikes":processed_dislikes,
+            "suscribed":processed_suscribed,
+            "teaches":proccessed_teaching,
+            "follows":proccessed_follows
         }
 
         txn.mutate(set_obj=user)
-
-        commit_response = txn.commit()
-        print(f"Commit Response: {commit_response}")
+        
+        txn.commit()
     except Exception as e:
-        print(f"Error during upsert: {e}")
+        print(f"Error during creation of user: {e}")
+    finally:
+        txn.discard()
+
+def upload_course(id,nameCourse,descriptionCourse,spaces,keywords,mode):
+    connection = DBconnections.DGRAPH
+    txn = connection.txn()
+
+    result = checkUID(id)
+
+    if result is not None:
+        course_uid = result
+    else:
+        course_uid = "_:new_obj"
+    try:
+        course = {
+            "uid":course_uid,
+            "c_uid":id,
+            "dgraph.type":"course",
+            "name":nameCourse,
+            "description":descriptionCourse,
+            "registration_spaces":spaces,
+            "keywords":keywords,
+            "mode":mode
+        }
+        txn.mutate(set_obj=course)
+        txn.commit()
+    except Exception as e:
+        print(f"Error during creation of course: {e}")
     finally:
         txn.discard()
 
@@ -127,6 +187,8 @@ def loadData():
     
     for user in data.get("users"):
         obtainUser(user)
+    for course in data.get("courses"):
+        obtainCourse(course)
 
 
 def deleteData():
